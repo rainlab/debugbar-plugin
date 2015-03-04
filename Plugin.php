d062a0fad584bc31da19b32329870bc35137956c
scottbedard <scottbedard90@gmail.com>
2015-03-03 19:51:25 -0500

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,44 @@
+<?php namespace Bedard\Debugbar;
+
+use App;
+use Event;
+use System\Classes\PluginBase;
+
+/**
+ * Debugbar Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Debugbar',
+            'description' => 'Debugbar integration for OctoberCMS.',
+            'author'      => 'Bedard',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register service provider and Twig extensions.
+     */
+    public function boot()
+    {
+        // Service provider
+        App::register('\Barryvdh\Debugbar\ServiceProvider');
+
+        // Twig extensions
+        Event::listen('cms.page.beforeDisplay', function($controller, $url, $page) {
+            $twig = $controller->getTwig();
+            $twig->addExtension(new \Barryvdh\Debugbar\Twig\Extension\Debug($this->app));
+            $twig->addExtension(new \Barryvdh\Debugbar\Twig\Extension\Stopwatch($this->app));
+        });
+    }
+
+}
