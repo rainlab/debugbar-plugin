94201604bb13b66e27ff9bd871d32d0ed0026939
Marko Kodrič <marko.kodric9@gmail.com>
2020-10-02 02:09:44 +0200

    Added Slovenian translations (#39)
    Co-authored-by: Ben Thomson <ben@abweb.com.au>

---
+++ lang/sl/lang.php
@@ -0,0 +1,8 @@
+<?php return [
+    'plugin' => [
+        'name'                   => 'Debugbar',
+        'description'            => 'Integracija PHP Debugbar knjižnice z October CMS.',
+        'access_debugbar'        => 'Dostop do razhroščevalne vrstice',
+        'access_stored_requests' => 'Dostop do shranjenih zahtev (nevarno)',
+    ],
+];
