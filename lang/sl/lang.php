6ad6220be3aadba57773602842b669c250a07fbf
Ben Thomson <ben@abweb.com.au>
2020-10-02 08:09:09 +0800

    Fix code style

--- lang/sl/lang.php
+++ lang/sl/lang.php
@@ -3,6 +3,6 @@
         'name'                   => 'Debugbar',
         'description'            => 'Integracija PHP Debugbar knjižnice z October CMS.',
         'access_debugbar'        => 'Dostop do razhroščevalne vrstice',
-        'access_stored_requests' => 'Dostop do shranjenih zahtev (nevarno)'
+        'access_stored_requests' => 'Dostop do shranjenih zahtev (nevarno)',
     ],
 ];
